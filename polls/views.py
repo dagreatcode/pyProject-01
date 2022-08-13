0bc21c7b1eb030593aac3059eae5c9afae94cd85
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-13 12:27:53 -0400

    added more view functions to views

--- polls/views.py
+++ polls/views.py
@@ -15,3 +15,13 @@ def test(request):
     r = requests.get('https://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
