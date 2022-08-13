ec51c87d71b73b370aaf445a9f2b51c69dc14059
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-13 03:01:45 -0400

    added a view

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import requests
 
 
 def index(request):
@@ -9,3 +10,8 @@ def index(request):
 
 def say_hello(request):
     return render(request, 'hello.html', {'name': 'DaGreat!'})
+
+def test(request):
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
